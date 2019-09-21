58a97db5ec6688166d09aaab695f9e2085227826
Henry AT <yiperu@gmail.com>
2019-09-21 11:06:25 -0300

    Test Failed - Make files to test and CreatureGenerator file, that is necessary for test

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/CreatureGenerator.kt
@@ -0,0 +1,8 @@
+package com.raywenderlich.android.creaturemon.model
+
+class CreatureGenerator {
+  fun generateCreature(attributes: CreatureAttributes, name: String, drawable: Int = 0): Creature {
+
+    return Creature()
+  }
+}
